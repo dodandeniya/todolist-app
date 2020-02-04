251136ab12ef55061159315c047637ba05bcf7b8
Chamara Dodandeniya <92chamara@gmail.com>
2020-02-04 20:55:35 +0530

    Adding ToDo List files

---
+++ src/TodoItems.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+    createTasks(item) {
+        return <li onClick={() => this.delete(item.key)}
+            key={item.key}>{item.text}</li>
+    }
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTasks);
+
+        return (
+            <ul className="theList">
+                <FlipMove duration={250} easing="ease-out">
+                    {listItems}
+                </FlipMove>
+            </ul>
+        );
+    }
+};
+
+export default TodoItems;
